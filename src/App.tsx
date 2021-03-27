fe57a54204a8cb4050d728ec377b65cebefb87aa
Cyrus <csarkosh@uw.edu>
2021-03-26 19:34:17 -0700

    Add title screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
 import React, {RefObject} from "react"
-import { Engine, Scene } from 'babylonjs'
+import {
+    Engine,
+    FreeCamera,
+    HemisphericLight,
+    Mesh,
+    Scene,
+    Vector3,
+} from 'babylonjs'
 
 export default class App extends React.Component {
     canvas: RefObject<HTMLCanvasElement> = React.createRef()
 
+    componentDidMount() {
+        const engine: Engine = new Engine(this.canvas.current, true, {
+            preserveDrawingBuffer: true,
+            stencil: true
+        })
+        const scene: Scene = new Scene(engine)
+        const camera: FreeCamera = new FreeCamera('user-camera', new Vector3(0, 5, -10), scene)
+        scene.render()
+    }
+
     render() {
         return (
-            <canvas
-                ref={this.canvas}
-            >
-                You need to use a browswer that supports
-                HTML5 canvas to access this game
-            </canvas>
+            <React.Fragment>
+                <canvas ref={this.canvas}>
+                    You need to use a browswer that supports HTML5 canvas to access this game.
+                </canvas>
+                <div id="menu">
+                    <div className="menu-content">
+                        <div className="title-text">Shooter</div>
+                        <button type="button" className="btn btn-light border shadow-sm d-block w-100 mt-2">Singleplayer</button>
+                        <button type="button" className="btn btn-light border shadow-sm d-block w-100 mt-2">Multiplayer</button>
+                        <div className="mt-2 d-flex">
+                            <button type="button" className="btn btn-light border shadow-sm w-100 me-1">Options</button>
+                            <button type="button" className="btn btn-light border shadow-sm w-100 ms-1">Credits</button>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 import "bootstrap/dist/css/bootstrap.min.css"
+import './index.scss';
+
 
 ReactDOM.render(
     <React.StrictMode>
