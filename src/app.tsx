b48875907ecacb9da9067bfd392d5d264c264169
Cyrus <csarkosh@uw.edu>
2021-05-08 14:27:33 -0700

    Begin separating game, network, and webpage code

--- src/app.tsx
+++ src/app.tsx
@@ -1,44 +1,73 @@
-import React, {RefObject} from "react"
-import {
-    Engine,
-    FreeCamera,
-    HemisphericLight,
-    Mesh,
-    Scene,
-    Vector3,
-} from 'babylonjs'
+import React from "react"
+import Game from "./game";
 
-export default class App extends React.Component {
-    canvas: RefObject<HTMLCanvasElement> = React.createRef()
+enum Menu {
+    MAIN,
+    NONE,
+    OPTIONS,
+    CREDITS,
+    SINGLEPLAYER,
+    MULTIPLAYER,
+}
 
-    componentDidMount() {
-        const engine: Engine = new Engine(this.canvas.current, true, {
-            preserveDrawingBuffer: true,
-            stencil: true
-        })
-        const scene: Scene = new Scene(engine)
-        const camera: FreeCamera = new FreeCamera('user-camera', new Vector3(0, 5, -10), scene)
-        scene.render()
+class App extends React.Component {
+    private canvas: React.RefObject<HTMLCanvasElement> = React.createRef()
+    private game: Game|null = null
+    state = {
+        menu: Menu.MAIN,
     }
-
+    launchGame = () => {
+        this.setMenu(Menu.NONE)
+        this.game = Game.create(this.canvas.current)
+    }
+    setMenu = (menu: Menu) => this.setState({ menu })
+    setMenuTransition = (menu: Menu) => () => this.setMenu(menu)
     render() {
         return (
             <React.Fragment>
                 <canvas ref={this.canvas}>
                     You need to use a browswer that supports HTML5 canvas to access this game.
                 </canvas>
-                <div id="menu">
+                <div id="menu" style={this.state.menu === Menu.NONE ? { display: 'none' } : undefined}>
                     <div className="menu-content">
                         <div className="title-text">Shooter</div>
-                        <button type="button" className="btn btn-light border shadow-sm d-block w-100 mt-2">Singleplayer</button>
-                        <button type="button" className="btn btn-light border shadow-sm d-block w-100 mt-2">Multiplayer</button>
-                        <div className="mt-2 d-flex">
-                            <button type="button" className="btn btn-light border shadow-sm w-100 me-1">Options</button>
-                            <button type="button" className="btn btn-light border shadow-sm w-100 ms-1">Credits</button>
-                        </div>
+                        <React.Fragment>
+                            <button
+                                className="btn btn-light border shadow-sm d-block w-100 mt-2"
+                                onClick={this.launchGame}
+                                type="button"
+                            >
+                                Singleplayer
+                            </button>
+                            <button
+                                className="btn btn-light border shadow-sm d-block w-100 mt-2"
+                                onClick={this.launchGame}
+                                type="button"
+                            >
+                                Multiplayer
+                            </button>
+                            <div className="mt-2 d-flex">
+                                <button
+                                    className="btn btn-light border shadow-sm w-100 me-1"
+                                    onClick={this.setMenuTransition(Menu.OPTIONS)}
+                                    type="button"
+                                >
+                                    Options
+                                </button>
+                                <button
+                                    className="btn btn-light border shadow-sm w-100 ms-1"
+                                    onClick={this.setMenuTransition(Menu.CREDITS)}
+                                    type="button"
+                                >
+                                    Credits
+                                </button>
+                            </div>
+                        </React.Fragment>
                     </div>
                 </div>
             </React.Fragment>
         )
     }
 }
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import App from './app';
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import './index.scss';
